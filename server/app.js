2ba61f23bb25c14bc2e7721ea33333c9e6107351
my-amtech <ammuftau74@gmail.com>
2023-01-05 18:44:51 +0100

    DB connected

--- server/app.js
+++ server/app.js
@@ -32,7 +32,11 @@ app.use(exp({
 }))
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:4000", "http://127.0.0.1:5500"],
+    credentials: true,
+  })
+);
 app.use(xss())
 
 //routes
